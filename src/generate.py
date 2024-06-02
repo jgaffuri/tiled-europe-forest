4ad94aef50a721567844b9940781eb53f6d66cf9
jgaffuri <jgaf@proton.me>
2024-06-02 17:24:47 +0200

    a

---
+++ src/generate.py
@@ -0,0 +1,23 @@
+from pygridmap import gridtiler_raster
+
+print("start")
+for res in [50000, 20000, 10000, 5000, 2000, 1000, 500, 200, 100]:
+    print(res)
+    tiling_raster(
+        {
+        "tcd_2012":{"file":'/home/juju/geodata/forest/in/forest_TCD_2012_100.tif', "band":1, 'no_data_values':[255,0]},
+        "tcd_2015":{"file":'/home/juju/geodata/forest/in/forest_TCD_2015_100.tif', "band":1, 'no_data_values':[255,0]},
+        "tcd_2018":{"file":'/home/juju/geodata/forest/in/forest_TCD_2018_100.tif', "band":1, 'no_data_values':[255,0]},
+        "dlt_2012":{"file":'/home/juju/geodata/forest/in/forest_DLT_2012_100.tif', "band":1, 'no_data_values':[255,0]},
+        "dlt_2015":{"file":'/home/juju/geodata/forest/in/forest_DLT_2015_100.tif', "band":1, 'no_data_values':[255,0]},
+        "dlt_2018":{"file":'/home/juju/geodata/forest/in/forest_DLT_2018_100.tif', "band":1, 'no_data_values':[255,0]}
+    },
+    "/home/juju/Bureau/forest/"+str(res)+"/",
+    res,
+    900000,
+    900000,
+    7400000,
+    5500000,
+    tile_size_cell=256,
+    format="parquet"
+    )
