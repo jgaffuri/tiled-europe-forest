d7dc1e7e104c13878ea67965d487b32cd2c7e981
jgaffuri <jgaf@proton.me>
2024-06-02 17:32:37 +0200

    a

--- src/generate.py
+++ src/generate.py
@@ -1,16 +1,19 @@
 from pygridmap import gridtiler_raster
 
+input_data_folder = "/home/juju/geodata/forest/in/"
+
 print("start")
 for res in [50000, 20000, 10000, 5000, 2000, 1000, 500, 200, 100]:
     print(res)
+
     gridtiler_raster.tiling_raster(
         {
-        "tcd_2012":{"file":'/home/juju/geodata/forest/in/forest_TCD_2012_100.tif', "band":1, 'no_data_values':[255,0]},
-        "tcd_2015":{"file":'/home/juju/geodata/forest/in/forest_TCD_2015_100.tif', "band":1, 'no_data_values':[255,0]},
-        "tcd_2018":{"file":'/home/juju/geodata/forest/in/forest_TCD_2018_100.tif', "band":1, 'no_data_values':[255,0]},
-        "dlt_2012":{"file":'/home/juju/geodata/forest/in/forest_DLT_2012_100.tif', "band":1, 'no_data_values':[255,0]},
-        "dlt_2015":{"file":'/home/juju/geodata/forest/in/forest_DLT_2015_100.tif', "band":1, 'no_data_values':[255,0]},
-        "dlt_2018":{"file":'/home/juju/geodata/forest/in/forest_DLT_2018_100.tif', "band":1, 'no_data_values':[255,0]}
+        "tcd_2012":{"file":input_data_folder+'forest_TCD_2012_100.tif', "band":1, 'no_data_values':[255,0]},
+        "tcd_2015":{"file":input_data_folder+'forest_TCD_2015_100.tif', "band":1, 'no_data_values':[255,0]},
+        "tcd_2018":{"file":input_data_folder+'forest_TCD_2018_100.tif', "band":1, 'no_data_values':[255,0]},
+        "dlt_2012":{"file":input_data_folder+'forest_DLT_2012_100.tif', "band":1, 'no_data_values':[255,0]},
+        "dlt_2015":{"file":input_data_folder+'forest_DLT_2015_100.tif', "band":1, 'no_data_values':[255,0]},
+        "dlt_2018":{"file":input_data_folder+'forest_DLT_2018_100.tif', "band":1, 'no_data_values':[255,0]}
     },
     "pub/parquet/"+str(res)+"/",
     res,
