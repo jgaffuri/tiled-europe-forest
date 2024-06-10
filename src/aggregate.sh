64dd0555ef520f973ab1f19c27a2e5b34ab634fe
jgaffuri <jgaf@proton.me>
2024-06-10 21:08:22 +0200

    a

--- src/aggregate.sh
+++ src/aggregate.sh
@@ -6,4 +6,5 @@ for year in "2012" "2015" "2018"; do
 
         gdalwarp -tr $res $res -r average -tap "/home/juju/geodata/forest/in/forest_TCD_"$year"_100.tif" /home/juju/geodata/forest/in/forest_TCD_"$year"_"$res".tif
         gdalwarp -tr $res $res -r mode -tap "/home/juju/geodata/forest/in/forest_DLT_"$year"_100.tif" /home/juju/geodata/forest/in/forest_DLT_"$year"_"$res".tif
+    done
 done
