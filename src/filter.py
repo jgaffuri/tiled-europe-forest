cd33e6e83941dfac8571276f0356b15c15babb20
jgaffuri <jgaf@proton.me>
2024-06-11 18:23:21 +0200

    a

--- src/filter.py
+++ src/filter.py
@@ -3,6 +3,7 @@ import numpy as np
 from scipy.ndimage import maximum_filter, binary_dilation
 
 
+
 # Function to expand the 255 values to their 8 neighbors
 def expand_255(data):
     # Create a mask of where the 255 values are
@@ -25,13 +26,7 @@ def expand_255(data):
 
 
 
-for year in [2012,2015,2018]:
-    print(year)
-
-    year = str(year)
-    input_file = '/home/juju/geodata/forest/forest_TCD_'+year+'_100_.tif'
-    output_file = '/home/juju/geodata/forest/forest_TCD_'+year+'_100.tif'
-
+def process(input_file, output_file):
     # Open the input GeoTIFF file
     with rasterio.open(input_file) as src:
         # Read the entire raster into a numpy array
@@ -39,10 +34,13 @@ for year in [2012,2015,2018]:
         
         # Modify the pixel values: set all values > 100 to 255
         data[data > 100] = 255
-        
+
         # Expand 255 values to their 8 neighbors
         data = expand_255(data)
-        
+
+        # Change 255 to 0
+        data[data > 100] = 0
+
         # Update the metadata to be the same as the source, but update the dtype if necessary
         profile = src.profile
         profile.update(dtype=rasterio.uint8)  # Ensure the data type matches the new values
@@ -50,3 +48,12 @@ for year in [2012,2015,2018]:
         # Write the modified data to a new GeoTIFF file
         with rasterio.open(output_file, 'w', **profile) as dst:
             dst.write(data, 1)
+
+
+
+for type in ["TCD", "DLT"]:
+    for year in [2012,2015,2018]:
+        year = str(year)
+
+        print(type, year)
+        process('/home/juju/geodata/forest/forest_'+type+'_'+year+'_100_.tif', '/home/juju/geodata/forest/forest_'+type+'_'+year+'_100.tif')
