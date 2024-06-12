cffe58952620260837bcb268c07e40fa5379c67c
jgaffuri <jgaf@proton.me>
2024-06-12 18:13:05 +0200

    a

--- src/tile.py
+++ src/tile.py
@@ -1,15 +1,15 @@
-from pygridmap import gridtiler_raster
-#from gridtiler_raster import tiling_raster
+#from pygridmap import gridtiler_raster
+from gridtiler_raster import tiling_raster
 
 input_data_folder = "/home/juju/geodata/forest/"
 
 print("start")
-for res in [50000, 20000, 10000, 5000, 2000, 1000, 500, 200, 100]:
-#for res in [1000, 500, 200, 100]:
+#for res in [50000, 20000, 10000, 5000, 2000, 1000, 500, 200, 100]:
+for res in [100]:
     print(res)
 
-    gridtiler_raster.tiling_raster(
-    #tiling_raster(
+    #gridtiler_raster.tiling_raster(
+    tiling_raster(
         {
         "tcd_2012":{"file":input_data_folder+'forest_TCD_2012_'+str(res)+'.tif', "band":1, 'no_data_values':[255,254,0]},
         "tcd_2015":{"file":input_data_folder+'forest_TCD_2015_'+str(res)+'.tif', "band":1, 'no_data_values':[255,254,0]},
