7be69329a83b026fa59ac914b4e937a0a920edd0
jgaffuri <jgaf@proton.me>
2024-06-12 18:15:09 +0200

    a

--- src/tile.py
+++ src/tile.py
@@ -3,6 +3,8 @@ from gridtiler_raster import tiling_raster
 
 input_data_folder = "/home/juju/geodata/forest/"
 
+# 5s per tile
+
 print("start")
 #for res in [50000, 20000, 10000, 5000, 2000, 1000, 500, 200, 100]:
 for res in [100]:
@@ -26,5 +28,5 @@ for res in [100]:
     5500000,
     tile_size_cell=256,
     format="parquet",
-    num_processors_to_use=8
+    num_processors_to_use=1
     )
