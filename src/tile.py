4d84f748fd158926a2b0ba515b68e54ddadf3183
jgaffuri <jgaf@proton.me>
2024-06-12 22:35:17 +0200

    a

--- src/tile.py
+++ src/tile.py
@@ -1,5 +1,5 @@
-#from pygridmap import gridtiler_raster
-from gridtiler_raster import tiling_raster_fast
+from pygridmap import gridtiler_raster
+#from gridtiler_raster import tiling_raster_fast
 
 input_data_folder = "/home/juju/geodata/forest/"
 
@@ -10,8 +10,8 @@ print("start")
 for res in [10000]:
     print(res)
 
-    #gridtiler_raster.tiling_raster(
-    tiling_raster_fast(
+    gridtiler_raster.tiling_raster(
+    #tiling_raster_fast(
         {
         "tcd_2012":{"file":input_data_folder+'forest_TCD_2012_'+str(res)+'.tif', "band":1, 'no_data_values':[255,254,0]},
         "tcd_2015":{"file":input_data_folder+'forest_TCD_2015_'+str(res)+'.tif', "band":1, 'no_data_values':[255,254,0]},
@@ -21,11 +21,11 @@ for res in [10000]:
         "dlt_2018":{"file":input_data_folder+'forest_DLT_2018_'+str(res)+'.tif', "band":1, 'no_data_values':[255,254,0]}
     },
     "./pub/parquet/"+str(res),
-    #res,
-    #900000,
-    #900000,
-    #7400000,
-    #5500000,
+    res,
+    900000,
+    900000,
+    7400000,
+    5500000,
     tile_size_cell=256,
     format="parquet",
     num_processors_to_use=1
