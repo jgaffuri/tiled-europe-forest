2508235c79b3d28c74730f1ad283d7b3a7d3e503
jgaffuri <jgaf@proton.me>
2024-06-13 15:47:18 +0200

    a

--- src/gridtiler_raster.py
+++ src/gridtiler_raster.py
@@ -98,7 +98,7 @@ def tiling_raster_fast(rasters, output_folder, crs="", tile_size_cell=128, forma
 
         #prepare raster data query window
         min_col = xt * tile_size_cell
-        min_row = width - 1 - 2*tile_size_cell
+        min_row = width - 1 - 3* tile_size_cell
         #col first, then row: window(col, row, w, h)
         window = rasterio.windows.Window(min_col, min_row, tile_size_cell, tile_size_cell)
         #print(min_col, min_row)
@@ -107,6 +107,7 @@ def tiling_raster_fast(rasters, output_folder, crs="", tile_size_cell=128, forma
         for key in keys:
             #print(key)
 
+            #get raster
             raster = rasters[key]
             src = raster["src"]
 
